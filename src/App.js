aba241d8dec1cbf899c5e77a39f630fc026190d6
Matthew Hawkins <mhawkins@basis.cloud>
2023-06-16 08:46:59 +0200

    init

--- src/App.js
+++ src/App.js
@@ -6,20 +6,63 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className='gyro'>GYROTONIC®</p>
+        <p>Ready to make your Gyrotonic website :)</p>Gyrotonic is a training system that combines fluid movements, breathing control and choreographed sequences to improve strength, flexibility, balance and body awareness.
+Developed in the 1980s by Romanian dancer Juliu Horvath, Gyrotonic integrates the principles of dance, yoga, Tai Chi, and swimming to create a unique approach to working out the body.
+The Gyrotonic is based on a specific device called the "Gyrotonic Expansion System", made up of pulleys, cables and mobile platforms which provide resistance and support for the body during movements.
+The holistic approach of Gyrotonic is one of the main pillars of this discipline which considers the body, mind and spirit as a single interconnected entity, promotes overall well-being and favors a better quality of life.
+
+        <p></p>
+        <ul>
+          <li>Freedom of Movement</li>
+          <li>Deep Breathing</li>
+          <li>Adapability</li>
+          <li>Healthy Routine</li>
+          <li>Global Well-being</li>
+        </ul>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// ReportVitals?
+// No border radius, Fede doesn't like it ;)
+
+
+
+/*
+ * Title
+ *
+ * #CEC3B5
+ *
+ * #E0E0E0
+ *
+ * #292726
+ * 
+ * #91A38B
+ * 
+ * 
+ * 
+ *
+ *
+ */
+
+
+/*
+ * Homepage - Brief intro and descrip of the team, the space, the methodology, etc
+ *
+ * Method - More in-depth description of the various exercises and reasoning - can add a video
+ * + 
+ * Hours and Price
+ *
+ * Biography page 
+ * +
+ * Contact 
+ * 
+ */
+
+/*
+ * Stationary canvas picture background at the bottom like ADP 
+ */
